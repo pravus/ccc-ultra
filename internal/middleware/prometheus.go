78b86308d71f1fbd78a2515f178bc376f0243496
Jason Hord <jason.hord@gmail.com>
2023-05-20 18:08:50 -0400

    updated prometheus wrapping

--- internal/middleware/prometheus.go
+++ internal/middleware/prometheus.go
@@ -8,40 +8,49 @@ import (
 )
 
 func Prometheus(label string) func(http.Handler) http.Handler {
-	counter := prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: `ultra_` + label + `_requests`,
-		Help: `HTTP Requests`,
-	}, []string{`code`, `method`})
-	prometheus.MustRegister(counter)
-	duration := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    `ultra_` + label + `_duration`,
-		Help:    `HTTP request duration`,
-		Buckets: []float64{.25, .5, 1, 2.5, 5, 10},
-	}, []string{`code`, `method`})
-	prometheus.MustRegister(duration)
-	inFlight := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: `ultra_` + label + `_in_flight`,
-		Help: `A gauge of requests currently in flight`,
-	})
-	prometheus.MustRegister(inFlight)
-	requestSize := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    `ultra_` + label + `_request_size`,
-		Help:    `HTTP request size`,
-		Buckets: []float64{200, 500, 900, 1500},
-	}, []string{})
-	prometheus.MustRegister(requestSize)
-	responseSize := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name:    `ultra_` + label + `_response_size`,
-		Help:    `HTTP response size`,
-		Buckets: []float64{200, 500, 900, 1500},
-	}, []string{})
-	prometheus.MustRegister(responseSize)
-	return func(next http.Handler) http.Handler {
-		return promhttp.InstrumentHandlerInFlight(inFlight,
-			promhttp.InstrumentHandlerDuration(duration,
-				promhttp.InstrumentHandlerCounter(counter,
-					promhttp.InstrumentHandlerResponseSize(responseSize,
-						promhttp.InstrumentHandlerRequestSize(requestSize, next),
-					))))
-	}
+	return func(
+		counter *prometheus.CounterVec,
+		duration prometheus.ObserverVec,
+		inFlight prometheus.Gauge,
+		requestSize prometheus.ObserverVec,
+		responseSize prometheus.ObserverVec,
+	) func(http.Handler) http.Handler {
+		prometheus.MustRegister(counter)
+		prometheus.MustRegister(duration)
+		prometheus.MustRegister(inFlight)
+		prometheus.MustRegister(requestSize)
+		prometheus.MustRegister(responseSize)
+		return func(next http.Handler) http.Handler {
+			return promhttp.InstrumentHandlerInFlight(inFlight,
+				promhttp.InstrumentHandlerDuration(duration,
+					promhttp.InstrumentHandlerCounter(counter,
+						promhttp.InstrumentHandlerResponseSize(responseSize,
+							promhttp.InstrumentHandlerRequestSize(requestSize, next),
+						))))
+		}
+	}(
+		prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: `ultra_` + label + `_requests`,
+			Help: `A counter of total requests`,
+		}, []string{`code`, `method`}),
+		prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    `ultra_` + label + `_duration`,
+			Help:    `A histogram of request duration`,
+			Buckets: []float64{.25, .5, 1, 2.5, 5, 10},
+		}, []string{`code`, `method`}),
+		prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: `ultra_` + label + `_in_flight`,
+			Help: `A gauge of requests currently in flight`,
+		}),
+		prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    `ultra_` + label + `_request_size`,
+			Help:    `A histogram of request size`,
+			Buckets: []float64{200, 500, 900, 1500},
+		}, []string{}),
+		prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    `ultra_` + label + `_response_size`,
+			Help:    `A histogram of response size`,
+			Buckets: []float64{200, 500, 900, 1500},
+		}, []string{}),
+	)
 }
