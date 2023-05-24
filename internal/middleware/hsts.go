ac623874172a99ef5ced63784928dd8f12f19014
Jason Hord <jason.hord@gmail.com>
2023-05-24 13:28:25 -0400

    fixed seconds conversion

--- internal/middleware/hsts.go
+++ internal/middleware/hsts.go
@@ -9,7 +9,7 @@ import (
 func Hsts(maxAge time.Duration) func (http.Handler) http.Handler {
 	return func (next http.Handler) http.Handler {
 		return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-			w.Header().Add(`Strict-Transport-Security`, fmt.Sprintf(`max-age=%d; includeSubDomains`, maxAge.Seconds()))
+			w.Header().Add(`Strict-Transport-Security`, fmt.Sprintf(`max-age=%d; includeSubDomains`, int64(maxAge / time.Second)))
 			next.ServeHTTP(w, r)
 		})
 	}
